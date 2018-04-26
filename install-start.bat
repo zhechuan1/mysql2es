d4535525e20948b6eb845da2f0ac7d751da7e5c9
justplay1994 <764804602@qq.com>
2018-04-26 13:42:55 +0800

    增加安装脚本，拷贝配置文件

--- install-start.bat
+++ install-start.bat
@@ -1,3 +1,4 @@
 
 call mvn clean package
-java -jar -Dfile.encoding=UTF-8  target/mysql2es-1.0-SNAPSHOT.jar F:\output\intellij\mysql2es\mysql2es.properties
+copy src\main\resources\mysql2es.properties mysql2es.properties
+java -jar -Dfile.encoding=UTF-8  target/mysql2es-1.0-SNAPSHOT.jar
