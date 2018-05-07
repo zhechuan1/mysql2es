e4d6a21cd16dc2c549c5ae95b7fa9f1f4a82e8b6
justplay1994 <764804602@qq.com>
2018-05-07 10:25:52 +0800

    新增启动脚本，将安装启动脚本进行拆分

--- install-start.bat
+++ install-start.bat
@@ -1,5 +1,6 @@
 call mvn clean package
 copy src\main\resources\mysql2es.properties target\mysql2es.properties
+copy start.bat target\start.bat
 cd target
-java -Xmx6144m  -Xms6144m -Xmn500m -Dfile.encoding=UTF-8 -jar mysql2es-1.0-SNAPSHOT.jar mysql2es.properties
+call ./start.bat
 pause
